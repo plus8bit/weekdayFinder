6dfb8d9ed8d3cd9133c31f807e4f337a2c663784
Nick <plus0bit@gmail.com>
2019-05-01 20:06:06 +0300

    Added the ability to find a weekday

--- WeekDayFinder/ViewController.swift
+++ WeekDayFinder/ViewController.swift
@@ -25,6 +25,27 @@ class ViewController: UIViewController {
     }
 
     @IBAction func findDayButton(_ sender: UIButton) {
+        let calendar = Calendar.current
+        
+        var dataComponents = DateComponents()
+        guard let day = dateTF.text, let month = monthTF.text, let year = yearTF.text else { return }
+        
+        dataComponents.day = Int(day)
+        dataComponents.month = Int(month)
+        dataComponents.year = Int(year)
+        
+        guard let date = calendar.date(from: dataComponents) else { return }
+        
+        let dateFormater = DateFormatter()
+        dateFormater.dateFormat = "EEEE"
+        
+        let weekDay = dateFormater.string(from: date)
+        
+        resultLabel.text = weekDay
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
     
 }
