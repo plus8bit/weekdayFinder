a812ce523847a39188b50f3542deb849f6baf75a
Nick <plus0bit@gmail.com>
2019-05-01 18:46:14 +0300

    Add basic UI

--- WeekDayFinder/ViewController.swift
+++ WeekDayFinder/ViewController.swift
@@ -9,12 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    
+    @IBOutlet weak var dateTF: UITextField!
+    @IBOutlet weak var monthTF: UITextField!
+    @IBOutlet weak var yearTF: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func findDayButton(_ sender: UIButton) {
+    }
+    
 }
 
